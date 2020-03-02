87370c19be607f898d67784ac3d1d521dbc68a32
Jurrian <jurrianlammerts@hotmail.com>
2020-03-02 20:40:19 +0100

    Initial Commit

---
+++ BeerFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeerFinder
+//
+//  Created by Jurrian Lammerts on 02/03/2020.
+//  Copyright © 2020 Jurrian Lammerts. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
